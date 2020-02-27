acfbbaff54904b63c67d7d43983ad0cd4a15854b
Jess Dam <jess.dam@starlingbank.com>
2020-02-27 10:05:20 +0000

    password hashing function

--- models/user/User.model.js
+++ models/user/User.model.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String
 })
+
+UserSchema.pre('save', function() {
+    const salt = bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashedPassword
+})
+
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = User
